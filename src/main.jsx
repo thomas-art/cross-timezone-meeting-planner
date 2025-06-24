608bb61d60eb73ac89ac641b56b0a8fa09f2e88c
thomas-art <thomaswang2016@163.com>
2025-06-24 13:55:35 +0800

    fix 404

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// 修复Leaflet默认marker图标路径
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
